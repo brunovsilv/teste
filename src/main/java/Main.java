c12a09cd8f9884d3020d4cadbe171a67925df3ee
bruno vinicius <33725261-brunocorsini2@users.noreply.replit.com>
2024-05-06 15:18:13 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Olá Mundo!");
+    System.out.print("Bruno Vinicius");
+    }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
